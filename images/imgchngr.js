bced6b94ca5ee556c12abb214081dd5934ffd736
mintyshoes <minsupark@live.com>
2015-12-04 12:38:44 -0500

    imgchanger changes

---
+++ images/imgchngr.js
@@ -0,0 +1,18 @@
+var imageArray = ['../images/workers/Barricade.jpg',
+                  '../images/workers/Bumblebee.jpg',
+                  '../images/workers/Fallen.jpg',
+                  '../images/workers/Megatron.jpg',
+								  '../images/workers/Optimus.jpg',
+								  '../images/workers/Ratchet.jpg',
+								  '../images/workers/SentinelPrime.jpg',
+								  '../images/workers/Starscream.jpg'];
+var imageIndex = 0;
+
+function nextImage(element) {
+    var image = document.getElementById(element);
+    imageIndex = (imageIndex + 1) % imageArray.length;
+    image.src = imageArray[imageIndex];
+};
+window.onload = function() {
+    setInterval(nextImage, 5000, "changing_image");
+};
